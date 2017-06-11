bce219d03eb4b71a2ab9657b9b9e0b5b3ee4782e
Gabriel Moral <gabrielmoralortiz@gmail.com>
2017-06-11 17:54:48 +0100

    First attempts in F#

--- Foo/Foo.fs
+++ Foo/Foo.fs
@@ -1,4 +1,13 @@
 namespace Foo
 
-type Foo() = 
-    member this.X = "F#"
+open System
+
+module Foo = 
+    let RemoveExclamationMarks (a : string) = a.Replace("!","")
+    let ToAcronim (s : string) = 
+
+        let extractFirstCharacter (s : string) = Char.ToUpper s.[0]
+
+        s.Split [|' '|]
+        |> Array.map extractFirstCharacter
+        |> System.String
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -2,7 +2,19 @@ module Tests
 
 open NUnit.Framework
 open FsUnit
+open Foo
 
 [<Test>]
-let ``Example Test`` () =
-    1 |> should equal 1
+let ``Remove exclamation marks`` () =
+    Foo.RemoveExclamationMarks "Hello World!" |> should equal "Hello World"
+    Foo.RemoveExclamationMarks "Hello Universe!" |> should equal "Hello Universe"
+
+[<Test>]
+let ``acronim`` () =
+    Foo.ToAcronim "Code Wars"  |> should equal "CW"
+    Foo.ToAcronim "Water Closet"  |> should equal "WC"
+    Foo.ToAcronim "Portable Network Graphics" |> should equal "PNG"  
+    Foo.ToAcronim "PHP: Hypertext Preprocessor"  |> should equal "PHP"
+    Foo.ToAcronim "hyper text markup language" |> should equal "HTML" 
+
+            
